4a507630a2d15eb3cc4694f3e87d5489caa8f363
Jon Forsyth <jon4syth@gmail.com>
2016-10-05 11:50:04 -0600

    add any_type function

--- 5_OCT_2016/value_types.exs
+++ 5_OCT_2016/value_types.exs
@@ -2,4 +2,27 @@ defmodule ValueTypes do
   def get_head_of_list(keyword_list) do
     # Enter Code
   end
+
+  def any_type(map) do
+
+  end
+
+  def any_type(tuple) do
+
+  end
+
+  def any_type(integer) do
+
+  end
+
+  def any_type(float) do
+
+  end
+
+  def any_type(atom) do
+
+  end
+
+  def any_type(list)
+
 end
