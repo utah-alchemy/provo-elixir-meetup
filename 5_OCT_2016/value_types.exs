0afdc8ec91ae84f4b41483505882404f524b968f
Jon Forsyth <jon4syth@gmail.com>
2016-10-06 19:32:28 -0600

    add any_type tests

--- 5_OCT_2016/value_types.exs
+++ 5_OCT_2016/value_types.exs
@@ -3,26 +3,34 @@ defmodule ValueTypes do
     # Enter Code
   end
 
-  def any_type(map) do
+  # use guard clauses to make the following functions match:
+  # Ex. def func(arg) when is_<type>(arg) do
+  #       "return this string"
+  #     end
 
+  def any_type(tuple) when is_tuple(tuple) do
+    # Code
   end
 
-  def any_type(tuple) do
+  # you can match a map by using an explicit map in the function signature
+  #def any_type(map) do
+    # Code
+  #end
 
-  end
+  #def any_type(integer) do
+    # Code
+  #end
 
-  def any_type(integer) do
+  #def any_type(float) do
+    # Code
+  #end
 
-  end
+  #def any_type(atom) do
+    # Code
+  #end
 
-  def any_type(float) do
-
-  end
-
-  def any_type(atom) do
-
-  end
-
-  def any_type(list)
+  #def any_type(list) do
+    # Code
+  #end
 
 end
--- 5_OCT_2016/value_types_test.exs
+++ 5_OCT_2016/value_types_test.exs
@@ -6,13 +6,43 @@ ExUnit.configure trace: true
 defmodule ValueTypesTest do
   use ExUnit.Case
 
-  #@tag :skip
+  @tag :skip
   test "the head of a keyword list is a tuple" do
 
     expected = {:a, 1}
     actual = ValueTypes.get_head_of_list([a: 1, b: 2, c: 3])
     IO.inspect(actual)
     assert actual == expected
+  end
+
+  @tag :skip
+  test "Returns `Map!` for tuple value type" do
+    assert ValueTypes.any_type(%{1 => 2}) == "Map!"
+  end
+
+  @tag :skip
+  test "Returns `Tuple!` for tuple value type" do
+    assert ValueTypes.any_type({1,2}) == "Tuple!"
+  end
+
+  @tag :skip
+  test "Returns `Integer!` for integer value type" do
+    assert ValueTypes.any_type(1) == "Integer!"
+  end
+
+  @tag :skip
+  test "Returns `Float!` for float value type" do
+    assert ValueTypes.any_type({1,2}) == "Float!"
+  end
+
+  @tag :skip
+  test "Returns `Atom!` for atom value type" do
+    assert ValueTypes.any_type(:atom) == "Atom!"
+  end
+
+  @tag :skip
+  test "Returns `List!` for list value type" do
+    assert ValueTypes.any_type({1,2}) == "List!"
   end
 
 end
