b42013e3b71a479886915842e4cae4a1f4c93465
Jon Forsyth <jon4syth@gmail.com>
2016-11-16 11:46:39 -0700

    add solution to first 3 problems

--- NOV_14_2016/modules.exs
+++ NOV_14_2016/modules.exs
@@ -1,12 +1,16 @@
 defmodule EagerProcess do
   def square(1..10_000_000 = range) do
-
+    range
+    |> Enum.map(fn n -> n * n end)
+    |> Enum.take(5)
   end
 end
 
 defmodule LazyProcess do
   def square(1..10_000_000 = range) do
-
+    range
+    |> Stream.map(fn n -> n * n end)
+    |> Enum.take(5)
   end
 end
 
--- NOV_14_2016/tests.exs
+++ NOV_14_2016/tests.exs
@@ -8,25 +8,33 @@ defmodule Tests do
   # Eager Processing
   #@tag :skip
   test "produce list of the squares 1-5 eagerly" do
-    assert EagerProcess.square() == [1,4,9,16,25]
+    assert EagerProcess.square(1..10_000_000) == [1,4,9,16,25]
   end
 
   # Lazy Processing
   #@tag :skip
   test "produce list of the squares 1-5 lazily" do
-    assert LazyProcess.square() == [1,4,9,16,25]
+    assert LazyProcess.square(1..10_000_000) == [1,4,9,16,25]
   end
 
   # Compare Eager and Lazy
   #@tag :skip
   test "verify that time to process eager is greater than lazy" do
     # user the :timer module (see README)
-    # eager_timer = ?
-    # lazy_timer = ?
-    assert eager_timer > lazy_timer
+    alias EagerProcess, as: Eager
+    alias LazyProcess, as: Lazy
+
+    {eager_time, _result} = :timer.tc(fn ->
+      Eager.square(1..10_000_000)
+    end)
+    {lazy_time, _result} = :timer.tc(fn ->
+      Lazy.square(1..10_000_000)
+    end)
+
+    assert eager_time > lazy_time
   end
 
   # Longest Collatz Sequence
-  #@tag :skip
+  @tag :skip
   test "verify longest Collatz sequence in N?"
 end
