6c17846c726d3603ad1c21f740c881fb6d855803
Jon Forsyth <jon4syth@gmail.com>
2017-12-01 09:34:12 -0700

    Update 5 Oct task

--- OCT_5_2016/value_types.exs
+++ OCT_5_2016/value_types.exs
@@ -1,35 +1,41 @@
 defmodule ValueTypes do
   def get_head_of_list(keyword_list) do
-    # Enter Code
+    # your code here
   end
 
-  # use guard clauses to make the following functions match:
+  # use guard clauses by adding the when + the is-type function after
+  # the functions closing parenthesis
+  # to make the following functions match:
   # Ex. def func(arg) when is_<type>(arg) do
   #       "return this string"
   #     end
 
-  def any_type(tuple) when is_tuple(tuple) do
-    "Tuple!"
-  end
+  # def any_type(tuple) do
+  #   "Tuple!"
+  # end
   # you can match a map by using an explicit map in the function signature
-  def any_type(%{}) do
-    # Code
-  end
+  # def any_type(%{}) do
+  #   "Map!"
+  # end
 
-  #def any_type(integer) do
-    # Code
-  #end
+  # def any_type(integer) do
+  #   "Integer!"
+  # end
 
-  #def any_type(float) do
-    # Code
-  #end
+  # def any_type(float) do
+  #   "Float!"
+  # end
 
-  #def any_type(atom) do
-    # Code
-  #end
+  # def any_type(atom) do
+  #   "Atom!"
+  # end
 
-  #def any_type(list) do
-    # Code
-  #end
+  # def any_type(list) do
+  #   "List!"
+  # end
+
+  # def any_type(string) do
+  #   "Binary!"
+  # end
 
 end
--- OCT_5_2016/value_types_test.exs
+++ OCT_5_2016/value_types_test.exs
@@ -6,7 +6,6 @@ ExUnit.configure trace: true
 defmodule ValueTypesTest do
   use ExUnit.Case
 
-  @tag :skip
   test "the head of a keyword list is a tuple" do
 
     expected = {:a, 1}
@@ -20,34 +19,34 @@ defmodule ValueTypesTest do
     assert ValueTypes.any_type(%{1 => 2}) == "Map!"
   end
 
-  #@tag :skip
+  @tag :skip
   test "Returns `Tuple!` for tuple value type" do
     assert ValueTypes.any_type({1,2}) == "Tuple!"
   end
 
-  #@tag :skip
+  @tag :skip
   test "Returns `Integer!` for integer value type" do
     assert ValueTypes.any_type(1) == "Integer!"
   end
 
-  #@tag :skip
+  @tag :skip
   test "Returns `Float!` for float value type" do
     assert ValueTypes.any_type(1.2) == "Float!"
   end
 
-  #@tag :skip
+  @tag :skip
   test "Returns `Atom!` for atom value type" do
     assert ValueTypes.any_type(:atom) == "Atom!"
   end
 
-  #@tag :skip
+  @tag :skip
   test "Returns `List!` for list value type" do
     assert ValueTypes.any_type([1,2]) == "List!"
   end
 
-  #@tag :skip
-  test "Returns `String!` for list value type" do
-    assert ValueTypes.any_type("string") == "String!"
+  @tag :skip
+  test "Returns `Binary!` for string value type" do
+    assert ValueTypes.any_type("string") == "Binary!"
   end
 
 end
